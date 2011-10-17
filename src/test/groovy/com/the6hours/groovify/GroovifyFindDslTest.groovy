73318aa82229cf8d694374bd5bb5ee47d9b9f547
Igor Artamonov <igor@artamonov.ru>
2011-10-18 00:20:34 +0700

    basic findWhere

---
+++ src/test/groovy/com/the6hours/groovify/GroovifyFindDslTest.groovy
@@ -0,0 +1,43 @@
+package com.the6hours.groovify
+
+import com.the6hours.groovify.testmodels.Car
+import com.googlecode.objectify.ObjectifyFactory
+import spock.lang.Specification
+
+/**
+ * 
+ * @author Igor Artamonov (http://igorartamonov.com)
+ * @since 18.10.11
+ */
+@WithGae
+class GroovifyFindDslTest extends Specification {
+
+    def setup() {
+        ObjectifyFactory ofy = new ObjectifyFactory()
+        Groovify groovify = new Groovify(
+                objectifyFactory: ofy
+        )
+        [Car].each {
+             ofy.register(it)
+             groovify.register(it)
+        }
+    }
+
+    def "Basic find"() {
+        setup:
+            ['2101', '2106', '2108'].each {
+                Car car = new Car(
+                        vendor: 'Vaz',
+                        model: it
+                )
+                car.save()
+            }
+        when:
+            List cars = Car.findWhere(['model': '2101'])
+        then:
+            cars != null
+            cars.size() == 1
+            cars[0].model == '2101'
+    }
+
+}
